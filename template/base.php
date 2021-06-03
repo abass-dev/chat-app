c44b1140b73c35c8059c4849c497c0536a2d2227
abass-bencheik <cheikabassben@gmail.com>
2021-06-03 14:54:58 +0000

    Update base.php

--- template/base.php
+++ template/base.php
@@ -1,6 +1,6 @@
 <div class="base" id="base">
   <div class="app">
-    <h1 class="app-title">FakarayApp</h1>
+    <h1 class="app-title">Chat App</h1>
    <!-- Import login.php from template -->
    <?php require "login.php" ?>
   </div>
